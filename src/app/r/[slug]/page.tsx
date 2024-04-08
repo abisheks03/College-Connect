be8e74a72ebc11f66026935bbd6d9e26232488d9
bigbrainer03 <95427891+bigbrainer03@users.noreply.github.com>
2024-04-09 00:03:11 +0530

    subreddits

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,43 @@
+import MiniCreatePost from '@/components/MinCreatePost'
+import { INFINITE_SCROLLING_PAGINATION_RESULTS } from '@/config'
+import { getAuthSession } from '@/lib/auth'
+import { db } from '@/lib/db'
+import { notFound } from 'next/navigation'
+
+interface pageProps{
+    params: {
+        slug: string
+    }
+}
+const page= async ({params}:pageProps) => {
+    const {slug} = params
+    const session = await getAuthSession()
+
+    const subreddit = await db.subreddit.findFirst({
+        where: {name: slug},
+        include:{
+            posts:{
+                include:{
+                    author: true,
+                    votes: true,
+                    comments: true,
+                    subreddit: true,
+                },
+
+                take: INFINITE_SCROLLING_PAGINATION_RESULTS
+            },
+        },
+    })
+
+    if(!subreddit) return notFound()
+
+    return <>
+        <h1 className='font-bold text-3xl md:text-4xl h-14'>
+            r/{subreddit.name}
+        </h1>
+        <MiniCreatePost session={session}/>
+        {/* TODO: Show posts in user feed */}
+    </>
+}
+
+export default page
