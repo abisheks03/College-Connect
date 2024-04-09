38ccca0df7cf95c7210f3fcbac83690f0c2df8ac
bigbrainer03 <95427891+bigbrainer03@users.noreply.github.com>
2024-04-09 12:23:13 +0530

    Image Upload

--- src/app/api/subreddit/unsubscribe/route.ts
+++ src/app/api/subreddit/unsubscribe/route.ts
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
     const body = await req.json()
     const { subredditId } = SubredditSubscriptionValidator.parse(body)
 
-    // check if user has already subscribed to subreddit
+    // check if user has already subscribed or not
     const subscriptionExists = await db.subscription.findFirst({
       where: {
         subredditId,
@@ -23,17 +23,22 @@ export async function POST(req: Request) {
       },
     })
 
-    if (subscriptionExists) {
-      return new Response("You've already subscribed to this subspace", {
-        status: 400,
-      })
+    if (!subscriptionExists) {
+      return new Response(
+        "You've not been subscribed to this subspace, yet.",
+        {
+          status: 400,
+        }
+      )
     }
 
-    // create subreddit and associate it with the user
-    await db.subscription.create({
-      data: {
-        subredditId,
-        userId: session.user.id,
+    // create subspace and associate it with the user
+    await db.subscription.delete({
+      where: {
+        userId_subredditId: {
+          subredditId,
+          userId: session.user.id,
+        },
       },
     })
 
@@ -45,7 +50,7 @@ export async function POST(req: Request) {
     }
 
     return new Response(
-      'Could not subscribe to subspace at this time. Please try later',
+      'Could not unsubscribe from subspace at this time. Please try later',
       { status: 500 }
     )
   }
---
+++ src/app/api/uploadthing/route.ts
@@ -0,0 +1,9 @@
+import { createNextRouteHandler } from "uploadthing/next";
+ 
+import { ourFileRouter } from "./core";
+ 
+
+export const { GET, POST } = createNextRouteHandler({
+  router: ourFileRouter,
+ 
+});
